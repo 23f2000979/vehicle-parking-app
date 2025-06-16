57c2f8fbc09c846159cb9c9d26363b1e3b11fe00
QuantumCoder <adwaitkeshari289@gmail.com>
2025-06-17 02:07:48 +0530

    Initialised a simple Flask app

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-# Hello Hii 
+from flask import Flask
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    @app.route('/')
+    def home():
+        return "Welcome to the Vehicle Parking App!"
+    app.run(debug=True)
+
